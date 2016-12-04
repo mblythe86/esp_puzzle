9f998d2210725e8bac1520b48a557b8e90e42917
Matthew Blythe <mblythester+git@gmail.com>
2016-12-03 20:12:41 -0700

    fleshing out phase 1 a bit more, and moving things to progmem as much as possible

--- esp_puzzle.ino
+++ esp_puzzle.ino
@@ -2,15 +2,12 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 
-void phase1_setup();
-void phase1_loop();
-
 const byte DNS_PORT = 53;
 IPAddress apIP(192, 168, 1, 1);
 DNSServer dnsServer;
 ESP8266WebServer webServer(80);
 
-String responseHTML = ""
+const char responseHTML[] PROGMEM = ""
   "<!DOCTYPE html><html><head><title>ESP8266 Puzzle</title></head><body>"
   "<h1>Oops!</h1><p>You seem to have reached an invalid page!</p></body></html>";
 
@@ -27,7 +24,7 @@ void setup() {
 
   // replay to all requests with same HTML
   webServer.onNotFound([]() {
-    webServer.send(200, "text/html", responseHTML);
+    webServer.send_P(200, "text/html", responseHTML);
   });
   webServer.begin();
   phase1_setup();
--- phase1.ino
+++ phase1.ino
@@ -34,15 +34,64 @@ const int alphabet[27][20] PROGMEM = {
 /*space*/ {7, 0,0,0,0,0,0,0}
 };
 
-char* message = "I LOVE PUZZLES\0";
+const char* message = "I LOVE PUZZLES";
+
+const char phase1_passHTML[] PROGMEM = ""
+  "<!DOCTYPE html><html><head><title>ESP8266 Puzzle</title></head><body>"
+  "<h1>Congrats!</h1><p>I hope you do love puzzles!  ;-)</p>"
+  "<p>You may <a href=\"/phase2\">continue</a></p></body></html>";
+
+const char phase1_prompt_headHTML[] PROGMEM = ""
+  "<!DOCTYPE html><html><head><title>ESP8266 Puzzle</title></head><body>";
+
+const char phase1_prompt_tailHTML[] PROGMEM = ""
+  "<h1>Hi There!</h1><p>I thought it would be kind of boring to just give you an unprogrammed board, so I've embedded a little game.</p>"
+  "<p>There's no reward at the end (aside from personal satisfaction), but to continue you need to enter the password.</p>"
+  "<p>Need a <a href='/phase1_hint'>hint</a>?</p>"
+  "<form method='GET' action='phase1'>"
+    "<input type='text' placeholder='password' name='password'/>"
+    "<br /><input type='submit' value='Go!'/></form>"
+  "</body></html>";
+
+const char phase1_hintHTML[] PROGMEM = ""
+  "<!DOCTYPE html><html><head><title>ESP8266 Puzzle</title></head><body>"
+  "<p>Hmm...this doesn't bode well.</p>"
+  "<p>Hopefully the answer will come in a <b>flash</b> of inspiration...</p>"
+  "<p><a href=\"/phase1\">Back?</a></p></body></html>";
+  
+void handle_phase1(){
+  if(webServer.args() > 0){
+    //we're trying a password!
+    String password = webServer.arg("password");
+    password.toUpperCase();
+    if(password == message){
+      webServer.send_P(200, "text/html", phase1_passHTML);
+    }
+    else{
+      webServer.send(200, "text/html", String(reinterpret_cast<const __FlashStringHelper *>(phase1_prompt_headHTML)) + "<p>Bad password</p>" + reinterpret_cast<const __FlashStringHelper *>(phase1_prompt_tailHTML));
+    }
+  }
+  else{
+    webServer.send(200, "text/html", String(reinterpret_cast<const __FlashStringHelper *>(phase1_prompt_headHTML)) + reinterpret_cast<const __FlashStringHelper *>(phase1_prompt_tailHTML));
+  }
+}
+
+void handle_phase1_hint(){
+  webServer.send_P(200, "text/html", phase1_hintHTML);
+}
 
 void phase1_setup(){
+  webServer.on("/", handle_phase1);
+  webServer.on("/phase1", handle_phase1);
+  webServer.on("/phase1_hint", handle_phase1_hint);
+  webServer.on("/generate_204", handle_phase1);  //Android captive portal. Maybe not needed. Might be handled by notFound handler.
+  webServer.on("/fwlink", handle_phase1);  //Microsoft captive portal. Maybe not needed. Might be handled by notFound handler.
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
 }
 
 void phase1_loop(){
   if((long)(millis() - next_transition) > 0){
-    //Serial.println(String("morsestate: ") + letter + "  " + letter_pos + "  " + millis() + "   " + next_transition);
+    Serial.println(String("morsestate: ") + letter + "  " + letter_pos + "  " + millis() + "   " + next_transition);
     next_transition = millis() + dit_len;
     letter_pos++;
     int letter_idx = (message[letter] == ' ') ? 26 : (message[letter] - 'A');
